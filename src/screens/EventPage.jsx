d6a9e7a5c03d955a04ca0117dc32df77a1b327bb
sarthak783891 <gsarthak783891@gmail.com>
2024-04-13 04:00:44 +0530

    event page done

--- src/screens/EventPage.jsx
+++ src/screens/EventPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { images } from '../assets/data';
 const EventPage = () => {
   const params = useParams();
   const eventId = params.id;
@@ -20,17 +21,19 @@ const EventPage = () => {
   }, []);    
 
   return (
-    <div>
-        <div>
-          <img src=""/>
-        </div>
-        <div>
-            <h1>{eventDetails.eventName}</h1>
-            <p>Description of {eventDetails.eventName}</p>
-            <p>Venue of {eventDetails.eventName}</p>
-            <button> Subscribe </button>
-        </div>
+    <div className="flex flex-col justify-center items-center h-[90vh]">
+    <div className="bg-white pb-6 rounded-lg  shadow-md ">
+      <div>
+        <img src={images[0]} alt="Event Image" className="w-full h-72 object-cover rounded-lg rounded-b-none pb-4" />
+      </div>
+      <div className="text-center px-4">
+        <h1 className="text-2xl font-bold">{eventDetails.eventName}</h1>
+        <p className="mb-2">Description of {eventDetails.eventName}</p>
+        <p className="mb-4">Venue of {eventDetails.eventName}</p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-full ">Subscribe</button>
+      </div>
     </div>
+  </div>
   )
 }
 
