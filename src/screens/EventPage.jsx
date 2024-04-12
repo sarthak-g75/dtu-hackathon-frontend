f6452fc0e0eec9cb0f2a7e4ebce5a5acf9ca8876
sarthak783891 <gsarthak783891@gmail.com>
2024-04-13 05:15:05 +0530

    event page

--- src/screens/EventPage.jsx
+++ src/screens/EventPage.jsx
@@ -1,5 +1,6 @@
 import  {  useEffect,useState } from 'react'
 import { useParams } from 'react-router-dom'
+import {images} from '../assets/data'
 const EventPage = () => {
   const params = useParams();
   const eventId = params.id;
@@ -70,23 +71,24 @@ const EventPage = () => {
   }, []);    
 
   return (
-    <div>
-        {message ? message : <></>}
-        <div>
-          <img src=""/>
-        </div>
-        <div>
-            <h1>{eventDetails.eventName}</h1>
-            <p>Description of {eventDetails.eventName}</p>
-            <p>Venue of {eventDetails.eventName}</p>
-            {
-              isSubscribed ? 
-              <button> Already Enrolled </button>
-              :
-              <button onClick={subscribeToEvent}> Subscribe </button>
-            }
-        </div>
+    <div className="flex flex-col justify-center items-center h-screen">
+    {message && <div>{message}</div>}
+    <div className="bg-white pb-8 rounded-lg rounded-b-none shadow-md">
+      <div>
+        <img src={images[0]} alt={'hello'} className="w-full h-64 object-cover rounded-lg mb-4" />
+      </div>
+      <div className="text-center px-4">
+        <h1 className="text-2xl font-bold">{eventDetails.eventName}</h1>
+        <p className="mb-2">Description of {eventDetails.eventName}</p>
+        <p className="mb-4">Venue of {eventDetails.eventName}</p>
+        {isSubscribed ? (
+          <button className="bg-gray-400 text-gray-800 px-4 py-2 rounded-lg cursor-not-allowed w-full" disabled>Already Enrolled</button>
+        ) : (
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-full" onClick={subscribeToEvent}>Subscribe</button>
+        )}
+      </div>
     </div>
+  </div>
   )
 }
 
