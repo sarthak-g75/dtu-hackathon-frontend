fd0ec4c9aaa1edc56c9fdf99ea6eaa30b27f7b72
sarthak783891 <gsarthak783891@gmail.com>
2024-04-13 05:05:07 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="relative h-screen top-0">
       
       <div
-        className="absolute inset-0 bg-cover bg-center"
+        className="absolute inset-0 bg-cover bg-center backd"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1435686858161-59da32dfd4b4?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
           filter: 'blur(8px)', // Apply blur effect
